02315c68738b72cba9c7256abe225d4cdb589fbf
BurakDrg <burakdurgunsu@hotmail.com>
2017-09-25 17:51:41 +0300

    Word Seach fix1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -79,7 +79,7 @@ public class App
     }
 
      public static boolean MergeSort(ArrayList<String> A, ArrayList<String> B, int count){
-	if(A.size()==0 || B.size()==0) return false;
+	//if(A.size()==0 || B.size()==0) return false;
 	for(int i = 0; i < A.size();i++){
 		if(A.get(i)==B.get(0))	count++;
 	}
